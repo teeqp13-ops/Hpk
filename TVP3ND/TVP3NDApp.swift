d5e7e17795f284125774f9be6e83e02509278abc
teeqp13-ops <teeqp13@gmail.com>
2026-07-27 08:21:06 +0300

    إضافة مصدر التطبيق

---
+++ TVP3ND/TVP3NDApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct TVP3NDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
